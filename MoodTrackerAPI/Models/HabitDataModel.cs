6ecbd4db0b4cc6663017894a44149813ef3fb3af
moa <larssonmoaml@gmail.com>
2021-11-16 20:04:07 +0100

    added habitdatamodel

---
+++ MoodTrackerAPI/Models/HabitDataModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoodTrackerAPI.Models
+{
+    public class HabitDataModel
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public int uID { get; set; }
+        public DateTime HDate { get; set; }
+        public bool Teeth { get; set; }
+        public bool Hair { get; set; }
+        public bool Skincare { get; set; }
+        public bool Plants { get; set; }
+        public bool Gym { get; set; }
+        public bool DailyCleaning { get; set; }
+        public bool Meds { get; set; }
+        public bool Pets { get; set; }
+
+
+    }
+}
